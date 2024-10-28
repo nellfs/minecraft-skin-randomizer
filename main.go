1c720bb4087ecf246eeb9271da729261f3d459e9
nellfs <pokycelit@gmail.com>
2024-10-28 20:25:26 -0300

    todo

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/nellfs/minecraft-skin-randomizer/skin"
 )
 
+//TODO:
+//make the first release
+//create config.json if it does not exist or the flag is not set, use an interactive setup
+//follow the config.json layer enabling
+//- skin preview in terminal?
+
 func main() {
 	skinManager, err := skin.SetupSkinManager()
 	if err != nil {
